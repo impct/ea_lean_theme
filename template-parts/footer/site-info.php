032c443d11ea13b001108838bf4a61f47883b752
Haryshwaran <haryshwaran@live.com>
2017-03-29 09:05:54 +0530

    Initial Commit

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( '/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Copyright 2017   ' . get_bloginfo('name','display'), 'twentyseventeen' ) ); ?></a>
+</div><!-- .site-info -->
